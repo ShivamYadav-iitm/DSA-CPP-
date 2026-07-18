7b2e18ce49046848c05297078ef55cbbadd1c466
ShivamYadav-iitm <gcshivam.00@gmail.com>
2026-07-18 13:58:39 +0530

    DAY 20 Done

--- DSA/Recurssion/binary_string.cpp
+++ DSA/Recurssion/binary_string.cpp
@@ -1,21 +1,37 @@
 #include<iostream>
 using namespace std;
 
-void binstr(string ans,int n,int lastplace){
+// void binstr(string ans,int n,int lastplace){
+//     if(n==0){
+//         cout<<ans<<endl;
+//         return;
+//     }
+//     if(lastplace != 1){
+//         binstr(ans+'0',n-1,0);
+//         binstr(ans+'1',n-1,1);
+//     }
+//     else{
+//         binstr(ans+'0',n-1,0);
+//     } 
+// }
+
+void binstr(string ans,int n){
     if(n==0){
         cout<<ans<<endl;
         return;
     }
-    if(lastplace != 1){
-        binstr(ans+'0',n-1,0);
-        binstr(ans+'1',n-1,1);
+    if(ans[ans.size()-1] != 1){
+        binstr(ans+'0',n-1);
+        binstr(ans+'1',n-1);
     }
     else{
-        binstr(ans+'0',n-1,0);
+        binstr(ans+'0',n-1);
     } 
 }
+
+
 int main(){
     string ans = "";
-    binstr(ans,6,0);
+    binstr(ans,6);
     return 0;
 }
---
+++ DSA/Recurssion/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+oid binstr(string ans,int n,int lastplace){
+//     if(n==0){
+//         cout<<ans<<endl;
+//         return;
+//     }
+//     if(lastplace != 1){
+//         binstr(ans+'0',n-1,0);
+//         binstr(ans+'1',n-1,1);
+//     }
+//     else{
+//         binstr(ans+'0',n-1,0);
+//     } 
+// }
